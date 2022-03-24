62709d895388002fd4f74f047386abd56534bcfc
CarlosRinconsofdev <3901@holbertonschool.com>
2022-03-24 18:28:30 -0500

    t13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,7 +8,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 size_t count = 0;
 
-if(tree == NULL)
+if (tree == NULL)
 return (0);
 if (tree->left != NULL || tree->right != NULL)
 count += 1;
@@ -16,5 +16,5 @@ count += 1;
 count += binary_tree_nodes(tree->left);
 count += binary_tree_nodes(tree->right);
 }
-return count;
+return (count);
 }
