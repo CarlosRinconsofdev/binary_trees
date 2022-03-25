8723bfd4ce1b2ceaf5829a9cf571339fc75d719d
CarlosRinconsofdev <3901@holbertonschool.com>
2022-03-24 21:46:05 -0500

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,16 +6,14 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-size_t left_height = 0;
-size_t right_height = 0;
+size_t left_height = 0, right_height = 0;
 
 if (tree == NULL)
 return (0);
 
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
-return (left_height);
-return (right_height);
+return (left_height - right_height);
 }
 
 /**
